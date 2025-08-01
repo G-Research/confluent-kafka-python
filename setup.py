e0499f0c23342c28e68b727c4fc19adff8ea6f61
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2025-08-01 16:22:37 +0100

    Create a universal build for linux-x64 using the system-provided sasl2 library

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import ctypes.util
 import os
 from setuptools import setup
 from setuptools import Extension
@@ -16,13 +17,44 @@ if platform.system() == 'Windows':
 else:
     librdkafka_libname = 'rdkafka'
 
-module = Extension('confluent_kafka.cimpl',
-                   libraries=[librdkafka_libname],
-                   sources=[os.path.join(ext_dir, 'confluent_kafka.c'),
-                            os.path.join(ext_dir, 'Producer.c'),
-                            os.path.join(ext_dir, 'Consumer.c'),
-                            os.path.join(ext_dir, 'Metadata.c'),
-                            os.path.join(ext_dir, 'AdminTypes.c'),
-                            os.path.join(ext_dir, 'Admin.c')])
+# Define the default module to build, without external dependencies.
+module_defs = [
+    {
+        'name': 'confluent_kafka.cimpl.nodeps.cimpl',
+        'libname': librdkafka_libname,
+    }
+]
 
-setup(ext_modules=[module])
+# Check for GSSAPI support and add the appropriate module definitions.
+if ctypes.util.find_library('rdkafka_sasl2_2'):
+    module_defs.append(
+        {
+            'name': 'confluent_kafka.cimpl.sasl2_2.cimpl',
+            'libname': 'rdkafka_sasl2_2',
+        }
+    )
+if ctypes.util.find_library('rdkafka_sasl2_3'):
+    module_defs.append(
+        {
+            'name': 'confluent_kafka.cimpl.sasl2_3.cimpl',
+            'libname': 'rdkafka_sasl2_3',
+        }
+    )
+
+setup(
+    ext_modules=[
+        Extension(
+            mod_def['name'],
+            libraries=[mod_def['libname']],
+            sources=[
+                os.path.join(ext_dir, 'confluent_kafka.c'),
+                os.path.join(ext_dir, 'Producer.c'),
+                os.path.join(ext_dir, 'Consumer.c'),
+                os.path.join(ext_dir, 'Metadata.c'),
+                os.path.join(ext_dir, 'AdminTypes.c'),
+                os.path.join(ext_dir, 'Admin.c'),
+            ],
+        )
+        for mod_def in module_defs
+    ]
+)
