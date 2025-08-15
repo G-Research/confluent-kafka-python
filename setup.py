ccaa5b89ad569b68cd5633b93516d0dfdcf94515
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2025-08-15 09:54:26 +0100

    Hotfix: use system-provided cyrus-sasl/libsasl2 at runtime (#10)
    - Hotfix: use system-provided cyrus-sasl/libsasl2 at runtime
      - revert previous approach for universal build that is now obsoleted by https://github.com/G-Research/librdkafka/pull/9
      - use `librdkafka.redist` version `2.11.0.1-RC1`
    - CI: build linux arm64 wheels too
    - CI: check version consistency
    - Bump version to `2.11.0.1-RC1+gr`

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import ctypes.util
 import os
 from setuptools import setup
 from setuptools import Extension
@@ -17,44 +16,13 @@ if platform.system() == 'Windows':
 else:
     librdkafka_libname = 'rdkafka'
 
-# Define the default module to build, without external dependencies.
-module_defs = [
-    {
-        'name': 'confluent_kafka.cimpl.nodeps.cimpl',
-        'libname': librdkafka_libname,
-    }
-]
+module = Extension('confluent_kafka.cimpl',
+                   libraries=[librdkafka_libname],
+                   sources=[os.path.join(ext_dir, 'confluent_kafka.c'),
+                            os.path.join(ext_dir, 'Producer.c'),
+                            os.path.join(ext_dir, 'Consumer.c'),
+                            os.path.join(ext_dir, 'Metadata.c'),
+                            os.path.join(ext_dir, 'AdminTypes.c'),
+                            os.path.join(ext_dir, 'Admin.c')])
 
-# Check for GSSAPI support and add the appropriate module definitions.
-if ctypes.util.find_library('rdkafka_sasl2_2'):
-    module_defs.append(
-        {
-            'name': 'confluent_kafka.cimpl.sasl2_2.cimpl',
-            'libname': 'rdkafka_sasl2_2',
-        }
-    )
-if ctypes.util.find_library('rdkafka_sasl2_3'):
-    module_defs.append(
-        {
-            'name': 'confluent_kafka.cimpl.sasl2_3.cimpl',
-            'libname': 'rdkafka_sasl2_3',
-        }
-    )
-
-setup(
-    ext_modules=[
-        Extension(
-            mod_def['name'],
-            libraries=[mod_def['libname']],
-            sources=[
-                os.path.join(ext_dir, 'confluent_kafka.c'),
-                os.path.join(ext_dir, 'Producer.c'),
-                os.path.join(ext_dir, 'Consumer.c'),
-                os.path.join(ext_dir, 'Metadata.c'),
-                os.path.join(ext_dir, 'AdminTypes.c'),
-                os.path.join(ext_dir, 'Admin.c'),
-            ],
-        )
-        for mod_def in module_defs
-    ]
-)
+setup(ext_modules=[module])
